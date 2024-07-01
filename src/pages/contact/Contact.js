2fdf2aaf3e646ed88ec3ffe006551f256cc842c8
Aymen Shoteri <aymenshoteri@gmail.com>
2024-07-01 16:57:21 -0400

    finished contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -5,8 +5,24 @@ import './Contact.scss';
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contact Page</h1>
-      <p>Contact us at this page.</p>
+        <div className="box">
+            <h1 className="title">Contact Us!</h1>
+                <form className="signin-form">
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" placeholder="Email"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="subject">Subject</label>
+                    <input type="subject" id="subject" name="subject" placeholder="Subject."/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message</label>
+                    <textarea className="message" id="message" name="message" placeholder="Message"></textarea>
+                </div>
+                <button type="submit">Send</button>
+            </form>
+        </div>
     </div>
   );
 }
